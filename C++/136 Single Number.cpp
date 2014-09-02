c6ff37e3cacdb7c86d9cf83646f6d3f022ae52fd
Li Long <1988lilong@163.com>
2014-09-02 16:59:56 +0800

    Create 136 Single Number.cpp

---
+++ C++/136 Single Number.cpp
@@ -0,0 +1,15 @@
+// https://oj.leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int res = 0;
+        
+        for(int i = 0; i < n; ++i)
+        {
+            res ^= A[i];
+        }
+        
+        return res;
+    }
+};
