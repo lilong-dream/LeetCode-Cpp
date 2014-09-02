34eb0d259cf992328221d61f0f88473b47773ccf
Li Long <1988lilong@163.com>
2014-09-02 17:16:51 +0800

    Update 135 Candy.cpp

--- C++/135 Candy.cpp
+++ C++/135 Candy.cpp
@@ -17,7 +17,7 @@ public:
         
         for(int i = n - 1; i > 0; --i)
         {
-            if((ratings[i - 1] > ratings[i]) && (candies[i - 1] <= candies[i]))
+            if((ratings[i - 1] > ratings[i]) && (candies[i - 1] <= candies[i]))  // !
             {
                 candies[i - 1] = candies[i] + 1;
             }
