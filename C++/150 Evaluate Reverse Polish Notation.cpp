ab384bb9888aa5302beed71673fa2beb1745ced0
Li Long <1988lilong@163.com>
2014-09-02 11:51:35 +0800

    Create 150 Evaluate Reverse Polish Notation.cpp

---
+++ C++/150 Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int evalRPN(vector<string> &tokens) {
+        std::stack<int> st;
+        
+        if(tokens.size() == 0)  
+        {
+            return 0;
+        }
+        
+        for(string str : tokens)
+        {
+            if((str == "+") || (str == "-") || (str == "*") || (str == "/"))
+            {
+                int op2 = st.top();
+                st.pop();
+                int op1 = st.top();
+                st.pop();
+                
+                if(str == "+")
+                {
+                    st.push(op1 + op2);
+                }
+                else if(str == "-")
+                {
+                    st.push(op1 - op2);
+                }
+                else if(str == "*")
+                {
+                    st.push(op1 * op2);
+                }
+                else
+                {
+                    st.push(op1 / op2);  // op2 == 0?
+                }
+            }
+            else
+            {
+                st.push(atoi(str.c_str()));
+            }
+        }
+        
+        return st.top();
+    }
+};
