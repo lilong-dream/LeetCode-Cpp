a89ac7e827197d5c8e2b0b5dc68847d1bb60cd00
Li Long <1988lilong@163.com>
2014-09-02 17:11:30 +0800

    Create 135 Candy.cpp

---
+++ C++/135 Candy.cpp
@@ -0,0 +1,34 @@
+// https://oj.leetcode.com/problems/candy/
+
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        int n = ratings.size();
+        
+        vector<int> candies(n, 1);
+        
+        for(int i = 0; i < n - 1; ++i)
+        {
+            if(ratings[i + 1] > ratings[i])
+            {
+                candies[i + 1] = candies[i] + 1; 
+            }
+        }
+        
+        for(int i = n - 1; i > 0; --i)
+        {
+            if((ratings[i - 1] > ratings[i]) && (candies[i - 1] <= candies[i]))
+            {
+                candies[i - 1] = candies[i] + 1;
+            }
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < n; ++i)
+        {
+            sum += candies[i];
+        }
+        
+        return sum;
+    }
+};
