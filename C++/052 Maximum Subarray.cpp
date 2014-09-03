9f4094102f2907f37e67cbf4c4d44a8304153f4e
Li Long <1988lilong@163.com>
2014-09-03 23:44:02 +0800

    Create 052 Maximum Subarray.cpp

---
+++ C++/052 Maximum Subarray.cpp
@@ -0,0 +1,29 @@
+// https://oj.leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        if(n == 0)
+        {
+            return 0;
+        }
+        
+        int max = INT_MIN;
+        int sum = 0;
+        
+        for(int i = 0; i < n; ++i)
+        {
+            sum += A[i];
+            if(sum > max)
+            {
+                max = sum;
+            }
+            if(sum < 0)
+            {
+                sum = 0;
+            }
+        }
+        
+        return max;
+    }
+};
