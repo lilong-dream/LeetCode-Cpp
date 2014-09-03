719f071fe70f2e7870f010ab10933bb13cc2c0d5
Li Long <1988lilong@163.com>
2014-09-03 23:29:24 +0800

    Create 074 Search a 2D Matrix.cpp

---
+++ C++/074 Search a 2D Matrix.cpp
@@ -0,0 +1,33 @@
+// https://oj.leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int m = matrix.size();
+        if(m == 0)
+        {
+            return false;
+        }
+        int n = matrix[0].size();
+        
+        int i = 0;
+        int j = n - 1;
+        while(i < m && j >= 0)
+        {
+            if(matrix[i][j] == target)
+            {
+                return true;
+            }
+            else if(matrix[i][j] > target)
+            {
+                --j;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+        
+        return false;
+    }
+};
