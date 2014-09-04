9b0ce250e117cad0e4f7ca15bcdeced7a8f80545
Li Long <1988lilong@163.com>
2014-09-04 10:33:29 +0800

    Create 069 Sqrt(x).cpp

---
+++ C++/069 Sqrt(x).cpp
@@ -0,0 +1,36 @@
+// https://oj.leetcode.com/problems/sqrtx/
+
+class Solution {
+public:
+    int sqrt(int x) {
+        if(x < 2)
+        {
+            return x;
+        }
+        
+        int left = 1, right = x / 2;
+        
+        int last = 0;
+        
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            if(x / mid > mid)
+            {
+                left = mid + 1;
+                last = mid;
+            }
+            else if(x / mid < mid)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        
+        return last;
+    }
+};
