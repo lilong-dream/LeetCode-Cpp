7e59ad95d824ae4fc78993b4576585c50d0a6294
Li Long <1988lilong@163.com>
2014-09-11 21:54:09 +0800

    Create 088 Merge Sorted Array.cpp

---
+++ C++/088 Merge Sorted Array.cpp
@@ -0,0 +1,27 @@
+// https://oj.leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while(i >= 0 && j >= 0)
+        {
+            if(A[i] > B[j])
+            {
+                A[k--] = A[i--];
+            }
+            else
+            {
+                A[k--] = B[j--];
+            }
+        }
+        
+        while(j >= 0)
+        {
+            A[k--] = B[j--];
+        }
+    }
+};
